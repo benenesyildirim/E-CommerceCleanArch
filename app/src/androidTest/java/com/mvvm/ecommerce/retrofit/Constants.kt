c7d5e856d6aea3105c08f3c98e195b840e338714
Enes Yıldırım <benenesyildirim@gmail.com>
2021-12-28 20:35:13 +0300

    - Retrofit test added.

---
+++ app/src/androidTest/java/com/mvvm/ecommerce/retrofit/Constants.kt
@@ -0,0 +1,48 @@
+package com.mvvm.ecommerce.retrofit
+
+object Constants {
+const val LISTING_SUCCESS_JSON = "[\n" +
+        "  {\n" +
+        "    \"id\": 1,\n" +
+        "    \"name\": \"Şampuan\",\n" +
+        "    \"price\": \"13\",\n" +
+        "    \"currency\": \"TRY\",\n" +
+        "    \"image\": \"https://cdn.glitch.com/a28552e7-44e1-4bbd-b298-5745e70c2209%2Fsampuan.jpeg?v=1561027551321\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"id\": 2,\n" +
+        "    \"name\": \"Deodorant\",\n" +
+        "    \"price\": \"26\",\n" +
+        "    \"currency\": \"TRY\",\n" +
+        "    \"image\": \"https://cdn.glitch.com/a28552e7-44e1-4bbd-b298-5745e70c2209%2Fdeodorant.jpeg?v=1561027551696\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"id\": 3,\n" +
+        "    \"name\": \"Diş Fırçası\",\n" +
+        "    \"price\": \"19.95\",\n" +
+        "    \"currency\": \"TRY\",\n" +
+        "    \"image\": \"https://cdn.glitch.com/a28552e7-44e1-4bbd-b298-5745e70c2209%2Fdis-fircasi.jpeg?v=1561027551798\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"id\": 4,\n" +
+        "    \"name\": \"Pahalı Şampuan\",\n" +
+        "    \"price\": \"135\",\n" +
+        "    \"currency\": \"TRY\",\n" +
+        "    \"image\": \"https://cdn.glitch.com/a28552e7-44e1-4bbd-b298-5745e70c2209%2Fsampuan.jpeg?v=1561027551321\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"id\": 5,\n" +
+        "    \"name\": \"Lüks Deodorant\",\n" +
+        "    \"price\": \"260\",\n" +
+        "    \"currency\": \"TRY\",\n" +
+        "    \"image\": \"https://cdn.glitch.com/a28552e7-44e1-4bbd-b298-5745e70c2209%2Fdeodorant.jpeg?v=1561027551696\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"id\": 6,\n" +
+        "    \"name\": \"Elit Diş Fırçası\",\n" +
+        "    \"price\": \"190.95\",\n" +
+        "    \"currency\": \"TRY\",\n" +
+        "    \"image\": \"https://cdn.glitch.com/a28552e7-44e1-4bbd-b298-5745e70c2209%2Fdis-fircasi.jpeg?v=1561027551798\"\n" +
+        "  }\n" +
+        "]"
+}
