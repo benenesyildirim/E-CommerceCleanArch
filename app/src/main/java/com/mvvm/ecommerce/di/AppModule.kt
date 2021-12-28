58397aa9623bf6eebea256faf3b7302e9c16672d
Enes Yıldırım <benenesyildirim@gmail.com>
2021-12-28 13:29:03 +0300

    - Designs are added. - Product and ProductDto data models added.

---
+++ app/src/main/java/com/mvvm/ecommerce/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.mvvm.ecommerce.di
+
+import com.mvvm.ecommerce.common.Constants.BASE_URL
+import com.mvvm.ecommerce.data.remote.ItemApi
+import com.mvvm.ecommerce.data.remote.repository.ProductRepositoryImpl
+import com.mvvm.ecommerce.domain.repository.ProductRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(): ItemApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ItemApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCharacterRepository(api: ItemApi): ProductRepository{
+        return ProductRepositoryImpl(api)
+    }
+}
